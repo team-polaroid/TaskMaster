1e7ad904b56f305f5f6194f1888b738fab98ac24
SDBogard56 <SDBogard56@gmail.com>
2019-02-27 10:25:20 -0600

    initial
    contains empty project template with runnable jar

---
+++ template/taskmaster/server/src/main/java/org/polaroid/taskmaster/TaskmasterApplication.java
@@ -0,0 +1,13 @@
+package org.polaroid.taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmasterApplication.class, args);
+    }
+
+}
